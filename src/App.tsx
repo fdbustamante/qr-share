72f4f2df303f90fc5a1a4db1d31a53c0daab416e
Federico Bustamante <fbustamante@despegar.com>
2021-08-05 20:09:29 -0300

    feat: generate qr

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import './App.css';
 
+import { Button, Divider, Grid, TextField } from '@material-ui/core';
 import React, { useState } from 'react';
 
 import QRCode from "react-qr-code";
 
 function App() {
-  const [text, setText] = useState("Hola");
+  const [value, setValue] = useState("Hola");
+
+  const [qrValue, setQrValue] = useState("Hola");
+
+  const handleChange = (event: any) => {
+    setValue(event.target.value);
+  };
 
   return (
-    <>
-      <input type="text" onChange={e => setText(e.target.value)}></input>
-      <QRCode value={text} size={256}/>
-    </>
+    <Grid style={{ margin: 20 }} container spacing={3}>
+      <Grid container item xs={6} spacing={3} justifyContent="center" alignItems="center" direction="column">
+        <Grid item xs={6}>
+          <TextField
+            id="standard-multiline-flexible"
+            label="Multiline"
+            multiline
+            maxRows={4}
+            value={value}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Button variant="contained" color="primary" onClick={() => setQrValue(value)} size="large">Generate QR</Button>
+        </Grid>
+      </Grid>
+      <Grid item xs={6}>
+        <QRCode value={qrValue} size={256}/>
+      </Grid>
+    </Grid>
   );
 }
 
