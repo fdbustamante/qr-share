89c3e46766f69a47b707bce7c58fab06f06f4e21
Federico Bustamante <fbustamante@despegar.com>
2021-08-05 19:09:58 -0300

    feat: qr code base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+import QRCode from "react-qr-code";
+
 function App() {
+  const [text, setText] = useState("Hola");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" onChange={e => setText(e.target.value)}></input>
+      <QRCode value={text} size={256}/>
+    </>
   );
 }
 
