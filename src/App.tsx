554355899b6a10758342e8121b171ebe527f396a
Federico Bustamante <fbustamante@despegar.com>
2021-08-06 00:58:07 -0300

    feat: grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
 
-import { Button, Divider, Grid, TextField, Theme, Typography, createStyles, makeStyles } from '@material-ui/core';
+import { Button, Container, Divider, Grid, Popover, TextField, Theme, Typography, createStyles, makeStyles } from '@material-ui/core';
 import React, { useState } from 'react';
 
 import { HexColorPicker } from "react-colorful";
+import LensIcon from '@material-ui/icons/Lens';
 import QRCode from "react-qr-code";
 import SendIcon from '@material-ui/icons/Send';
 
@@ -12,6 +13,12 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       flexGrow: 1,
     },
+    centerItem: {
+      textAlign: "center",
+    },
+    buttonSize: {
+      minWidth: 200,
+    }
   }),
 );
 
@@ -20,44 +27,82 @@ function App() {
 
   const [value, setValue] = useState("");
   const [qrValue, setQrValue] = useState("Hola Mundo!");
-  const [color, setColor] = useState("#aabbcc");
+  const [qrColor, setQrColor] = useState("#000000");
+  const [color, setColor] = useState("#000000");
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
 
   const handleChange = (event: any) => {
     setValue(event.target.value);
   };
 
+  const generateQr = () => {
+    setQrValue(value);
+    setQrColor(color);
+  }
+
   return (
-    <div className={classes.root}>
-      <Typography variant="h1" component="h2" gutterBottom align="center">
+    <Container className={classes.root}>
+      <Typography variant="h1" component="h2" gutterBottom align="center" style={{ marginTop: 20 }}>
         QR Code Generator
       </Typography>
       <Divider />
-      <Grid style={{ padding: 20 }} container spacing={8}>
-        <Grid container item xs={12} lg={6} spacing={8} justifyContent="center" alignItems="center" direction="column">
-          <Grid item xs>
+      <Grid style={{ padding: 20, marginTop: 20 }} container spacing={8}>
+        <Grid item xs={12} md={6} container spacing={0}>
+          <Grid item xs={12}>
             <TextField
               id="standard-multiline-flexible"
               label="Insert text"
               multiline
-              maxRows={4}
+              rows={4}
               value={value}
               onChange={handleChange}
               variant="outlined"
-              style={{ width: 400 }}
+              fullWidth
             />
-          </Grid>
-          <Grid item xs>
-          <HexColorPicker color={color} onChange={setColor} />
-            <Button variant="contained" color="primary" onClick={() => setQrValue(value)} size="large" endIcon={<SendIcon />}>Generate QR</Button>
-          </Grid>
+            </Grid>
+            <Grid item xs={12} container spacing={0}>
+            <Grid item xs={12} md={6} className={classes.centerItem} style={{marginTop: 20}} >
+
+            <Button className={classes.buttonSize} size="large" aria-describedby="colorpicker-id" variant="outlined" onClick={handleClick}  endIcon={<LensIcon style={{color: color}} />}>
+              Select Color
+            </Button>
+            <Popover
+              id="colorpicker-id"
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+            >
+              <HexColorPicker color={color} onChange={setColor} />
+            </Popover>
+            </Grid>
+            <Grid item xs={12} md={6} className={classes.centerItem} style={{marginTop: 20}} >
+            <Button className={classes.buttonSize} variant="contained" color="primary" onClick={generateQr} size="large" endIcon={<SendIcon />}>Generate QR</Button>
+            </Grid>
+            </Grid>
         </Grid>
-        <Grid container item xs={12} lg={6} spacing={8} justifyContent="center" alignItems="center" direction="column">
-          <Grid item xs={12} lg={6}>
-            <QRCode value={qrValue} size={256} fgColor={color}/>
-          </Grid>
+        <Grid item xs={12} md={6} className={classes.centerItem}>
+            <QRCode value={qrValue} size={256} fgColor={qrColor} />
         </Grid>
       </Grid>
-    </div>
+    </Container>
   );
 }
 
