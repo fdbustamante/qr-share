ca9bd7e9fb01d281be7b24b80e1204467d876a10
Federico Bustamante <fbustamante@despegar.com>
2021-08-06 14:58:38 -0300

    feat: commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,39 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'jest': true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'project': './tsconfig.json',
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 12,
-        'sourceType': 'module'
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    'plugins': [
-        'react',
-        '@typescript-eslint',
-        'react-hooks',
-        'prettier'
-    ],
-    'rules': {
-      indent: ['error', 2, { SwitchCase: 1 }],
-      quotes: ['error', 'single', { avoidEscape: true }],
-      'no-empty-function': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      'react/display-name': 'off',
-      'react/prop-types': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
-      'prettier/prettier': 'error',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
+  },
 };
