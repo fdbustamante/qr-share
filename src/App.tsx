01334c39bf2df803a4948ec87cbcd9c6014fc331
Federico Bustamante <fbustamante@despegar.com>
2021-08-05 23:12:10 -0300

    feat: color picker

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Button, Divider, Grid, TextField, Theme, Typography, createStyles, makeStyles } from '@material-ui/core';
 import React, { useState } from 'react';
 
+import { HexColorPicker } from "react-colorful";
 import QRCode from "react-qr-code";
 import SendIcon from '@material-ui/icons/Send';
 
@@ -18,8 +19,9 @@ function App() {
   const classes = useStyles();
 
   const [value, setValue] = useState("");
-
   const [qrValue, setQrValue] = useState("Hola Mundo!");
+  const [color, setColor] = useState("#aabbcc");
+
   const handleChange = (event: any) => {
     setValue(event.target.value);
   };
@@ -27,7 +29,7 @@ function App() {
   return (
     <div className={classes.root}>
       <Typography variant="h1" component="h2" gutterBottom align="center">
-        QR code online generator
+        QR Code Generator
       </Typography>
       <Divider />
       <Grid style={{ padding: 20 }} container spacing={8}>
@@ -45,12 +47,13 @@ function App() {
             />
           </Grid>
           <Grid item xs>
+          <HexColorPicker color={color} onChange={setColor} />
             <Button variant="contained" color="primary" onClick={() => setQrValue(value)} size="large" endIcon={<SendIcon />}>Generate QR</Button>
           </Grid>
         </Grid>
         <Grid container item xs={12} lg={6} spacing={8} justifyContent="center" alignItems="center" direction="column">
           <Grid item xs={12} lg={6}>
-            <QRCode value={qrValue} size={256}/>
+            <QRCode value={qrValue} size={256} fgColor={color}/>
           </Grid>
         </Grid>
       </Grid>
